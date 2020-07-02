7d0998b0b847af7cfb5deb1c47c377c6effae585
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-02 15:57:49 +0300

    Initial Commit

---
+++ DisclosureGroupInSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DisclosureGroupInSwiftUI
+//
+//  Created by ramil on 02.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisclosureGroupInSwiftUI/DisclosureGroupInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisclosureGroupInSwiftUIApp.swift
+//  DisclosureGroupInSwiftUI
+//
+//  Created by ramil on 02.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct DisclosureGroupInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
