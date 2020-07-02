0361ba8174d4dc64365e43959e6c0f1bf5ef44e2
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-02 16:09:06 +0300

    init

--- DisclosureGroupInSwiftUI/ContentView.swift
+++ DisclosureGroupInSwiftUI/ContentView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animalsExpanded: Bool = true
+    let animals = ["🐛", "🐍", "🦖", "🦑", "🦐"]
+    
+    @State private var fruitsExpanded: Bool = false
+    let fruits = ["🍓", "🍑", "🍐", "🍌", "🍊"]
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        Form {
+            DisclosureGroup(
+                isExpanded: $animalsExpanded,
+                content: {
+                    VStack {
+                        ForEach(animals, id: \.self) { animal in
+                            Text(animal)
+                                .font(.system(size: 50))
+                        }
+                    }
+                },
+                label: { Text("Animals") }
+            )
+            
+            DisclosureGroup(
+                isExpanded: $fruitsExpanded,
+                content: {
+                    VStack {
+                        ForEach(fruits, id: \.self) { fruit in
+                            Text(fruit)
+                                .font(.system(size: 50))
+                        }
+                    }
+                },
+                label: { Text("Fruits") }
+            )
+        }
     }
 }
 
